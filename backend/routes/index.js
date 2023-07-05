fd77862ab4abb82b722f615838c956fa754335c2
Liqin Li <liqinli.ehs@gmail.com>
2023-07-05 13:35:53 -0700

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,10 @@
 
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+// all the URLs of the routes in the api router will be prefixed with /api
+router.use('/api', apiRouter);
 
 // Add a XSRF-TOKEN cookie -> allow any developer to re-set the CSRF token cookie XSRF-TOKEN
 router.get("/api/csrf/restore", (req, res) => {
