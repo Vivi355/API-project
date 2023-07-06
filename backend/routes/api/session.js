12795635491a2b7f3fc74532ded97defbbc935f7
Liqin Li <liqinli.ehs@gmail.com>
2023-07-06 16:04:54 -0700

    Add user input validation on user login backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -5,10 +5,27 @@ const bcrypt = require('bcryptjs');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+// validating login request body
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 const router = express.Router();
 
+// check keys and validate them
+// check whether or not req.body.credential and req.body.password are empty
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
+
 // Log in
-router.post('/', async (req, res, next) => {
+router.post('/', validateLogin, async (req, res, next) => {
     const { credential, password } = req.body;
 
     const user = await User.unscoped().findOne({
@@ -40,7 +57,6 @@ router.post('/', async (req, res, next) => {
       return res.json({
         user: safeUser
       });
-
 });
 
 // log out --> remove token cookie from the response and return message
@@ -65,5 +81,7 @@ router.get('/',(req, res) => {
       } else return res.json({ user: null });
     }
 );
+
+
 
 module.exports = router;
