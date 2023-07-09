03f259808e2f45b684d02cfe6258098bcb5f0441
Liqin Li <liqinli.ehs@gmail.com>
2023-07-09 10:55:29 -0700

    added spotimage model and seed

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -7,6 +7,9 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       Spot.belongsTo(models.User, {
         foreignKey: 'ownerId'
+      }),
+      Spot.hasMany(models.SpotImage, {
+        foreignKey: 'spotId'
       })
     }
   }
