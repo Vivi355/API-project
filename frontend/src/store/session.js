36aeaad12e6a2c8e3d723fc259387a41597f2a95
Liqin Li <liqinli.ehs@gmail.com>
2023-07-28 13:03:20 -0700

    signup thunk added

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -4,7 +4,7 @@ import { csrfFetch } from "./csrf";
 const SET_USER = 'session/setUser';
 const REMOVE_USER = 'session/removeUser';
 
-/** action creator */
+/********************** action creator ***************************/
 const setUser = (user) => {
     return {
         type: SET_USER,
@@ -18,7 +18,7 @@ const removeUser = () => {
     }
 }
 
-/** thunk creator */
+/******************** thunk creator *******************************/
 export const login = (user) => async dispatch => {
     const {credential, password} = user;
     const res = await csrfFetch('api/session', {
@@ -42,7 +42,26 @@ export const restoreUser = () => async dispatch => {
     return res;
 }
 
-/** Reducer */
+// user sign up thunk
+export const signup = (user) => async dispatch => {
+    const {username, firstName, lastName, email, password} = user;
+    const res = await csrfFetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({
+            username,
+            firstName,
+            lastName,
+            email,
+            password,
+        }),
+    });
+
+    const data = await res.json();
+    dispatch(setUser(data.user));
+    return res;
+}
+
+/************************** Reducer ****************************/
 const initialState = {user: null};
 
 const sessionReducer = (state = initialState, action) => {
